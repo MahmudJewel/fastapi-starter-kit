70e5257ef646036ab5d96c8a666dc474dd12c1fb
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-02-04 12:05:46 -0500

    uuid removed

--- app/models/common.py
+++ app/models/common.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, Boolean, Integer, DateTime, func
+from sqlalchemy import Column, Boolean, Integer, String , DateTime, func
+import uuid
 from app.core.database import Base
 
 
 class CommonModel(Base):
     __abstract__ = True
 
+    # id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
     id = Column(Integer, primary_key=True, index=True)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
