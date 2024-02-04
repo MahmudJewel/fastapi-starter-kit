dd1bedfd7304049e73be5d138916018fbcf60e49
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-02-04 12:21:15 -0500

    added admin panel

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 from app.api.routers.user import user_router
 
+from sqladmin import Admin, ModelView
+from app.core.database import engine
+from app.models.user import User
+
 app = FastAPI()
 
 @app.get('/')
@@ -10,4 +14,10 @@ async def read_home_page():
 
 
 app.include_router(user_router)
+
+admin = Admin(app, engine)
+class UserAdmin(ModelView, model=User):
+    column_list = [User.id, User.email]
+
+admin.add_view(UserAdmin)
 
