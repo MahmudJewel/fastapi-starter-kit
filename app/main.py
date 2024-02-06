ee5c16d07d040bf33ca18f3ec8784a171dc33789
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-02-06 09:12:44 -0500

    added alembic and middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # fastapi
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # sqlalchemy
 from sqladmin import Admin, ModelView
@@ -10,6 +11,22 @@ from app.models.admin import UserAdmin
 
 
 app = FastAPI()
+
+origins = [
+	# "http://localhost.tiangolo.com",
+	# "https://localhost.tiangolo.com",
+	# "http://localhost",
+	# "http://localhost:8080",
+    "*"
+]
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 
 @app.get("/")
