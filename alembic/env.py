97851201b5e3c70aa50f786a91a190c2693cc902
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-02-10 15:04:52 -0500

    configured multiple table in alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.core.database import Base
 from app.models.user import User, metadata
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
